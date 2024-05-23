41b351fd6b725200773963b04d5145943a27ccc8
hyeonjun.lee <little6523@naver.com>
2024-05-23 15:08:44 +0900

    refactor: Show more 기능 수정 - 기존: fetch - 수정: ajax

--- Front_End_Project.js
+++ Front_End_Project.js
@@ -1,11 +1,28 @@
 const showMore = document.querySelector('#showMore');
 const picturesGrid = document.querySelector('.Main2-Pictures-Grid');
 
+// showMore.addEventListener('click', function () {
+//     fetch("https://picsum.photos/v2/list?page=2&limit=100")
+//         .then((response) => response.json())
+//         .then((data) => {
+//             for (let i = 0; i < 3; i++) {
+//                 let randomNumber = Math.floor(Math.random() * 100);
+//                 let imageUrl = data[randomNumber]['download_url'];
+//                 let newImg = document.createElement('img');
+//                 newImg.classList.add("Main2-Pictures");
+//                 newImg.setAttribute("src", imageUrl);
+//                 picturesGrid.appendChild(newImg);
+//             }
+//         });
+// })
+
 showMore.addEventListener('click', function (){
-    fetch("https://picsum.photos/v2/list?page=2&limit=100")
-        .then((response) => response.json())
-        .then((data) => {
-            for(let i=0; i < 3; i++) {
+    $.ajax({
+        url: "https://picsum.photos/v2/list?page=2&limit=100",
+        type: "get",
+        datatype: "json",
+        success: function (data) {
+            for (let i = 0; i < 6; i++) {
                 let randomNumber = Math.floor(Math.random() * 100);
                 let imageUrl = data[randomNumber]['download_url'];
                 let newImg = document.createElement('img');
@@ -13,20 +30,23 @@ showMore.addEventListener('click', function (){
                 newImg.setAttribute("src", imageUrl);
                 picturesGrid.appendChild(newImg);
             }
-        });
+        },
+        error: function (){
+            alert('error');
+        }
+    })
 })
-
 
 const subscribe = document.querySelector('#subscribe');
 const modal = document.querySelector('.modal')
 const modalOK = document.querySelector('#Modal_OK');
 
-subscribe.addEventListener('click', function (){
+subscribe.addEventListener('click', function () {
     console.log("모달창열기");
     modal.style.visibility = 'visible';
 })
 
-modalOK.addEventListener('click', function (){
+modalOK.addEventListener('click', function () {
     console.log("모달창닫기");
     modal.style.visibility = 'hidden';
 })
